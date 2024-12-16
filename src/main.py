2b2f5f7cc596904f4e732aadb2cd6f67fbea2d4f
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2024-12-17 01:17:40 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -18,7 +18,7 @@ logger = logging.getLogger(__name__)
 
 app = Flask(__name__)
 
-
+@app.route("/", methods=["POST"])
 def analyze_terraform_plan(request: Request):
     try:
         # Validate request
