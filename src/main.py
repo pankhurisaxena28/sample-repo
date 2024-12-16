eb8bbbd605ae6bcbb0a47f3a647219c06293a213
pankhurisaxena28 <76627925+pankhurisaxena28@users.noreply.github.com>
2024-12-16 16:08:10 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -22,87 +22,84 @@ app = Flask(__name__)
 @app.route("/")
 def analyze_terraform_plan(request: Request):
     try:
-        # Validate request
-        # is_valid, error_message = validate_request(request)
-        # if not is_valid:
-        #     return Response(error_message, status=400)
+        Validate request
+        is_valid, error_message = validate_request(request)
+        if not is_valid:
+            return Response(error_message, status=400)
 
-        # payload = request.get_json()
+        payload = request.get_json()
 
-        # if payload["stage"] == "test":
-        #     return Response("", status=200)
+        if payload["stage"] == "test":
+            return Response("", status=200)
 
-        # gcloud_access_token, project_id = get_access_token()
-        # print(f"{gcloud_access_token}")
-        # if gcloud_access_token is None:
-        #     logger.error("Failed to obtain access token")
-        #     return Response("Failed to obtain access token", status=500)
+        gcloud_access_token, project_id = get_access_token()
+        print(f"{gcloud_access_token}")
+        if gcloud_access_token is None:
+            logger.error("Failed to obtain access token")
+            return Response("Failed to obtain access token", status=500)
 
-        # if project_id is None:
-        #     logger.error("Failed to fetch project ID")
-        #     return Response("Failed to fetch project ID", status=500)
+        if project_id is None:
+            logger.error("Failed to fetch project ID")
+            return Response("Failed to fetch project ID", status=500)
 
-        # project_number = get_project_number(project_id)
-        # if project_number is None:
-        #     logger.error("Failed to fetch project number")
-        #     return Response("Failed to fetch project number", status=500)
+        project_number = get_project_number(project_id)
+        if project_number is None:
+            logger.error("Failed to fetch project number")
+            return Response("Failed to fetch project number", status=500)
 
-        # organization_id = get_organization_id(project_id, gcloud_access_token)
+        organization_id = get_organization_id(project_id, gcloud_access_token)
 
-        # if organization_id is None:
-        #     logger.error("Failed to fetch organization ID")
-        #     return Response("Internal server error", status=500)
+        if organization_id is None:
+            logger.error("Failed to fetch organization ID")
+            return Response("Internal server error", status=500)
 
-        # # Authenticate request
-        # if not validate_hmac_signature(request, project_number):
-        #     logger.error("Invalid HMAC signature")
-        #     return Response("Invalid HMAC signature", status=401)
+        # Authenticate request
+        if not validate_hmac_signature(request, project_number):
+            logger.error("Invalid HMAC signature")
+            return Response("Invalid HMAC signature", status=401)
 
-        # plan_file, status_code = fetch_terraform_plan(
-        #     payload["plan_json_api_url"], payload["access_token"]
-        # )
+        plan_file, status_code = fetch_terraform_plan(
+            payload["plan_json_api_url"], payload["access_token"]
+        )
 
-        # if status_code != 200:
-        #     logger.error("Failed to fetch plan file")
-        #     return Response("Failed to fetch plan file", status_code)
+        if status_code != 200:
+            logger.error("Failed to fetch plan file")
+            return Response("Failed to fetch plan file", status_code)
 
-        # iacv_operation_id, status_code, error_message = validate_iac(
-        #     plan_file, organization_id, gcloud_access_token, project_id
-        # )
+        iacv_operation_id, status_code, error_message = validate_iac(
+            plan_file, organization_id, gcloud_access_token, project_id
+        )
 
-        # if status_code != 200:
-        #     logger.error(error_message)
-        #     return Response(error_message, status_code)
-        # if error_message is not None:
-        #     logger.error(error_message)
-        #     return Response(error_message, 500)
+        if status_code != 200:
+            logger.error(error_message)
+            return Response(error_message, status_code)
+        if error_message is not None:
+            logger.error(error_message)
+            return Response(error_message, 500)
 
-        # iacv_report, status_code, error_message = fetch_iac_validation_report(
-        #     iacv_operation_id,
-        #     gcloud_access_token,
-        #     project_id,
-        #     payload["task_result_callback_url"],
-        #     payload["access_token"],
-        # )
+        iacv_report, status_code, error_message = fetch_iac_validation_report(
+            iacv_operation_id,
+            gcloud_access_token,
+            project_id,
+            payload["task_result_callback_url"],
+            payload["access_token"],
+        )
 
-        # if status_code != 200:
-        #     logger.error(error_message)
-        #     return Response(error_message, status_code)
-        # if error_message is not None:
-        #     logger.error(error_message)
-        #     return Response(error_message, 500)
+        if status_code != 200:
+            logger.error(error_message)
+            return Response(error_message, status_code)
+        if error_message is not None:
+            logger.error(error_message)
+            return Response(error_message, 500)
 
-        # callback_request = create_task_result_callback_request(iacv_report, status=None)
-        # callback_status_code = send_terraform_callback(
-        #     payload["task_result_callback_url"],
-        #     payload["access_token"],
-        #     callback_request,
-        # )
+        callback_request = create_task_result_callback_request(iacv_report, status=None)
+        callback_status_code = send_terraform_callback(
+            payload["task_result_callback_url"],
+            payload["access_token"],
+            callback_request,
+        )
 
-        # return callback_status_code
-
-        name = os.environ.get("NAME", "World")
-        return f"Hello {name}!"
+        return callback_status_code
 
 
     except Exception as e:
